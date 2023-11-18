ba3a7c559ed2525ef5604b4c77e63ca1feb7474c
kanagarla <ramakishore.kanagarla@sensfix.com>
2023-11-18 20:30:33 +0530

    My First code checkin Under Local Repository

---
+++ TestGit1/src/test/java/mypackage/Test1.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=10,b=20;
+			System.out.println("Result is:"+(a+b));
+	}
+
+}
