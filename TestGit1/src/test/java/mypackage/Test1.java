1c10563d169a9468310c26e1218692567c4a5966
kanagarla <ramakishore.kanagarla@sensfix.com>
2023-11-18 20:40:40 +0530

    Created new variable to store the addition of values

--- TestGit1/src/test/java/mypackage/Test1.java
+++ TestGit1/src/test/java/mypackage/Test1.java
@@ -4,8 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int a=10,b=20;
-			System.out.println("Result is:"+(a+b));
+			int a=10,b=20,c;
+			c=a+b;
+			System.out.println("Result is:"+c);
 	}
 
 }
